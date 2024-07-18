7f17aff21f1356cb0f341807f4babbf2b1b41879
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-07-18 11:09:38 -0400

    adding cors to backend

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from .routes import main
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     app.register_blueprint(main)
-
     return app
